18538aab99632c73f601c0fca980c116808253f2
Rodgerick Heninger <rodgerick.williams@icloud.com>
2022-04-20 15:49:26 -0600

    Redirect function

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,16 +1,12 @@
 import React from "react";
 import "../../App.css";
 import "./PagesStyles.css";
-import { Link } from "react-router-dom";
+import { Link , Redirect } from "react-router-dom";
 
 export default function About() {
   return (
     <>
-      <div className="sign-up">
-        <Link to="/">
-          <button class="styled-btn--outline">TO MAIN PAGE</button>
-        </Link>
-      </div>
+      <Redirect to="/" />
     </>
   );
 }
